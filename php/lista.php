8a31f28d73cbb8d99b41f886636ef5f754140e8e
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-15 15:25:12 +0200

    Update lista.php
    muokkaus

--- php/lista.php
+++ php/lista.php
@@ -13,6 +13,7 @@ include "../html/header.html";
 $tulos=mysqli_query($yhteys, "select * from vieraat");
 while ($rivi=mysqli_fetch_object($tulos)){
     print "<p>$rivi->vieras $rivi->teksti <a href='./poistavieraat.php?poistettava=$rivi->id'>Poista</a></p>";
+    "<a href='./muokkaa.php?muokattava=$rivi->id'>muokkaa</a>";
 }
 mysqli_close($yhteys);
 ?>
