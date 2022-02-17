40c5dd475e8bfade00c686932a85b4a93e8ee424
tejalyy <tea.lyytinen@student.hamk.fi>
2022-02-17 15:31:42 +0200

    Update vieraskirja.php
    muutos

--- php/vieraskirja.php
+++ php/vieraskirja.php
@@ -18,12 +18,12 @@ catch(Exception $e){
     exit;
 }
 
-$sql="insert into vieraat (vieras, teksti, p‰iv‰m‰‰r‰) values(?, ?, ?)";
+$sql="insert into vieraat (vieras, teksti) values(?, ?, ?)";
 
 //Valmistellaan sql-lause
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'ssi', $vieras, $teksti, $p‰iv‰m‰‰r‰);
+mysqli_stmt_bind_param($stmt, 'ss', $vieras, $teksti);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 //Suljetaan tietokantayhteys
