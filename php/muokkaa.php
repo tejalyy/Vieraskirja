448e60051caac1a06c735f24fd23c7d2bc02dcad
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-17 13:00:30 +0200

    Update muokkaa.php
    muutoksia

--- php/muokkaa.php
+++ php/muokkaa.php
@@ -1,34 +1,35 @@
 <?php 
 $muokattava=isset($_GET["muokattava"]) ? $_GET["muokattava"] : 0;
 if (empty($muokattava)) {
-header("Localtion;../luekirjat.php");
+header("Location;../lista.php");
 exit;
 }
 try {
-    $yhteys=mysql_connect("db", "root", "password", "kirjakanta");
+    $yhteys=mysql_connect("db", "root", "password", "vieraskirja");
 }
 catch(Exception $e) {
     header("location:.../html/yhteysvirhe.html");
     exit;
 }
-$sql="select * from kirja where id=?";
+$sql="select * from vieraat where id=?";
 $stmt=mysqli_prepare($yhteys, $sql);
 mysql_stmt_bind_param($stmt, 'i', $muokattava);
 mysql_stmt_execute($stmt);
 $tulos=mysqli_stmt_get_result($stmt);
 
 if (!$rivi=mysqli_fetch_object($tulos)) {
-    header("location:./luekirjat.php");
+    header("location:./lista.php");
     exit;
 }
 include "../html/header.html";
-}?>
+?>
 <p>Muokkaa tietoja</p>
-<from action='./paviata.php' method='post'>
+<form action='../php/vieraskirja.php' method='post'>
 <input type='hidden' name='id' value='<?php print $rivi->id;?>'><br>
-nimi: <input type='text' name='nimi' value='<?php print $rivi->nimi;?>'><br>
-sivulkm: <input type='text' name='sivulkm' value='<?php print $rivi->sivulkm;?>'><br>
-<input type='submit' name ='ok' value='OK'><br>
+P‰iv‰m‰‰r‰: <input type='date' name='date' value='<?php print $rivi->p‰iv‰m‰‰r‰;?>'><br>
+Nimi: <input type='text' name='vieras' value='<?php print $rivi->nimi;?>'><br>
+Viesti: <textarea type='text' name='teksti' value='<?php print $rivi->viesti;?>'></textarea><br>
+<input type='submit' name ='ok' value='L‰het‰'><br>
 </from>
 
 <?php 
