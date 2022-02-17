37d9a3285840f72356ca425ec57018fd180a6e15
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-17 18:51:15 +0200

    Update vieraskirja.php
    muutos

--- php/vieraskirja.php
+++ php/vieraskirja.php
@@ -23,7 +23,7 @@ $sql="insert into vieraat (vieras, teksti, p‰iv‰m‰‰r‰) values(?, ?, ?)";
 //Valmistellaan sql-lause
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'ss', $vieras, $teksti, $p‰iv‰m‰‰r‰);
+mysqli_stmt_bind_param($stmt, 'ssi', $vieras, $teksti, $p‰iv‰m‰‰r‰);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 //Suljetaan tietokantayhteys
