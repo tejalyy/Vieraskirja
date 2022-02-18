0701a06befbac68f2db0cbf065939c3e187f8de6
tejalyy <tea.lyytinen@student.hamk.fi>
2022-02-18 16:09:12 +0200

    Update lista.php
    muokkaa.php

--- php/lista.php
+++ php/lista.php
@@ -11,8 +11,7 @@ include "../html/header.html";
 echo "<div style=\"text-align:center\">";
 $tulos=mysqli_query($yhteys, "select * from vieraat");
 while ($rivi=mysqli_fetch_object($tulos)){
-    print "<p>$rivi->vieras $rivi->teksti $rivi->date <a href='./poista.php?poistettava=$rivi->id'>Poista</a></p>". 
-    "<a href='./muokkaa.php?muokattava=$rivi->id'>Muokkaa</a>";
+    print "<li>$rivi->id $rivi->vieras $rivi->teksti $rivi->date <a href='./poista.php?poistettava=$rivi->id'>Poista</a> <a href='./muokkaa.php?muokattava=$rivi->id'>Muokkaa</a>";
 }
 mysqli_close($yhteys);
 echo "</div>";
