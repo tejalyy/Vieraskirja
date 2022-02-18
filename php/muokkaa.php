c12ab368661707ddbccceb13590aa1efb32cfdb6
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-18 16:00:35 +0200

    css
    add css to muokkaa.php

--- php/muokkaa.php
+++ php/muokkaa.php
@@ -23,7 +23,8 @@ if (!$rivi=mysqli_fetch_object($tulos)) {
 }
 include "../html/header.html";
 ?>
-
+<link rel='stylesheet' type='text/css' href='../ css/tyyli.css'>
+<div class="col-3">
 <p><center>Muokkaa tietoja</center></p>
 <form action='./paivita.php' method='post'><center>
 <input type='hidden' name='id' value='<?php print $rivi->id;?>'><br>
@@ -32,6 +33,7 @@ Nimi: <input type='text' name='vieras' value='<?php print $rivi->nimi;?>'><br>
 Viesti: <textarea type='text' name='teksti' value='<?php print $rivi->viesti;?>'></textarea><br>
 <input type='submit' name ='ok' value='Lähetä'><br>
 </center></from>
+</div>
 
 <?php 
 mysqli-close($yhteys);
