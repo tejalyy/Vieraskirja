052e0a8dce9ba4a33174c11b5582737e55c617a9
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-20 20:35:45 +0200

    Update muokkaa.php
    add margin top

--- php/muokkaa.php
+++ php/muokkaa.php
@@ -22,6 +22,7 @@ if (!$rivi=mysqli_fetch_object($tulos)) {
     exit;
 }
 include "../html/header.html";
+echo "<div style=\"margin-top:-5.8%\">";
 ?>
 <link rel='stylesheet' type='text/css' href='../ css/tyyli.css'>
 <div class="col-3">
