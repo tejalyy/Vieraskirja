4176f822aaba11c367e70e06631a68b72748eabc
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-02-20 21:28:20 +0200

    muokkaa.php
    text-align center

--- php/muokkaa.php
+++ php/muokkaa.php
@@ -26,14 +26,13 @@ echo "<div style=\"margin-top:-5.8%\">";
 ?>
 <link rel='stylesheet' type='text/css' href='../ css/tyyli.css'>
 <div class="col-3">
-<p><center>Muokkaa tietoja</center></p>
-<form action='./paivita.php' method='post'><center>
+<p>Muokkaa tietoja</p>
+<form action='./paivita.php' method='post'>
 <input type='hidden' name='id' value='<?php print $rivi->id;?>'><br>
 Date: <input type='date' name='date' value='<?php print $rivi->date;?>'><br>
 Nimi: <input type='text' name='vieras' value='<?php print $rivi->vieras;?>'><br>
 Teksti: <textarea type='text' name='teksti' value='<?php print $rivi->teksti;?>'></textarea><br>
-<input type='submit' name ='ok' value='Tallenna'><br>
-</center></from>
+<input type='submit' name ='ok' value='Tallenna'><br></form>
 </div>
 
 <?php 
